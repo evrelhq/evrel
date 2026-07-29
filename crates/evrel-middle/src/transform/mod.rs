65471e3388ef6b5ed4826fd39b91b9f7e3a91ee7
Ish Chhabra <ishchhabra12@gmail.com>
2026-07-29 23:29:22 +0530

    feat(middle): eliminate dead code
    GitOrigin-RevId: 0332f369a2e3f9986cad5f01a82e34b5810a4fd4

--- crates/evrel-middle/src/transform/mod.rs
+++ crates/evrel-middle/src/transform/mod.rs
@@ -1,8 +1,10 @@
 //! Transformations over Evrel IR.
 
+mod eliminate_dead_code;
 mod promote_bindings_to_ssa;
 mod propagate_constants;
 mod ssa_updater;
 
+pub use eliminate_dead_code::eliminate_dead_code;
 pub use promote_bindings_to_ssa::promote_bindings_to_ssa;
 pub use propagate_constants::propagate_constants;
